4330396a14af84dc8457455d1504ebc0b7126317
praveen <149228650+Praveenkumarreddy12@users.noreply.github.com>
2024-10-29 09:33:07 +0530

    Create ezy.py

---
+++ ezy.py
@@ -0,0 +1,54 @@
+
+'''for x in range(5):
+    for y in range(0,x+1):
+        print("*",end="")
+    print()'''
+
+'''for x in range(5):
+    for y in range(0,5-x):
+        print("*",end="")
+    print()'''
+
+'''a=65
+for i in range(5):
+    for j in range(0,i+1):
+        print(chr(a),end="")
+        a+=1
+    print()'''
+
+
+'''for i in range (5):
+    a=65
+    for j in range (0,i+1):
+        print(chr(a),end="")
+        a=a+1
+    print()'''
+'''a=65
+for i in range(5):
+    for j in range(i+1):
+        print(chr(a),end="")
+        
+    a+=1
+    print()'''
+
+'''a,b=65,97
+for i in range(5):
+    for j in range(0,i+1):
+        print(chr(a)+chr(b),end=" ")
+        a+=1
+        b+=1
+    print()'''
+a,b=65,97
+
+for i in range(5):
+    
+    for y in range(0,i+1):
+        print(chr(a)+chr(b),end=" ")
+    a+=1
+    b+=2   
+    print()
+        
+
+
+
+
